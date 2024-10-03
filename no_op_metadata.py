418edd9e1ae39a8b062e7c7f747ec8416d9b7f94
paexternal <paexternal380@oaisb.info>
2024-10-03 00:01:01 +0000

    Added docstring for NoOpMetadata class in no_op_metadata.py

--- no_op_metadata.py
+++ no_op_metadata.py
@@ -5,5 +5,6 @@ from openfeature.provider.metadata import Metadata
 
 @dataclass
 class NoOpMetadata(Metadata):
+    """Represents a metadata class with no operation."""
     name: str = "No-op Provider"
     is_default_provider: bool = True
